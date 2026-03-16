e483a0b7adf9e4143e31068fda16ca25b376980e
kmlpiekarz <kamil.piekarz@applover.pl>
2026-03-16 12:04:49 +0100

    Bump version to 0.8.0

--- Sources/OpenWearablesHealthSDK/OpenWearablesHealthSDK.swift
+++ Sources/OpenWearablesHealthSDK/OpenWearablesHealthSDK.swift
@@ -30,7 +30,7 @@ public final class OpenWearablesHealthSDK: NSObject, URLSessionDelegate, URLSess
     /// Shared singleton instance.
     public static let shared = OpenWearablesHealthSDK()
     
-    internal static let sdkVersion = "0.7.0"
+    internal static let sdkVersion = "0.8.0"
     
     // MARK: - Public Callbacks
     
